95236ff9bafe84e452b1ce82cf05b9204632c066
Ryan L <ryan.lindeman711@gmail.com>
2020-10-11 00:33:13 -0400

    New Commit - 10/11 - Ryan

--- app/src/main/java/com/rkl/andriod1/details/ForecastDetailsActivity.kt
+++ app/src/main/java/com/rkl/andriod1/details/ForecastDetailsActivity.kt
@@ -1,15 +1,58 @@
 package com.rkl.andriod1.details
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rkl.andriod1.R
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.rkl.andriod1.*
+import kotlinx.android.synthetic.main.activity_forecast_details.view.*
 
 class ForecastDetailsActivity : AppCompatActivity() {
+
+    private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast_details)
 
+        tempDisplaySettingManager = TempDisplaySettingManager(this)
+
         setTitle(R.string.forecast_details)
+
+        val tempText = findViewById<TextView>(R.id.tempText)
+        val descriptionText = findViewById<TextView>(R.id.descriptionText)
+
+
+        val temp = intent.getFloatExtra("key_temp", 0f)
+
+        tempText.text = formatTempForDisplay(temp, tempDisplaySettingManager.getTempDisplaySetting())
+
+        descriptionText.text = intent.getStringExtra("key_description")
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Handle item selection
+        return when(item.itemId){
+            R.id.tempDisplaySetting -> {
+                //do something
+                //Toast.makeText(this, "clicked menu item", Toast.LENGTH_SHORT).show()
+                showTempDisplaySettingDialog(this, tempDisplaySettingManager)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
